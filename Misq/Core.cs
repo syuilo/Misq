708e44c95ae9b175e46a5efc3df8c9345d1b6163
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2018-09-29 21:45:13 +0900

    Update Core.cs

--- Misq/Core.cs
+++ Misq/Core.cs
@@ -1,8 +1,9 @@
-﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Misq
 {
